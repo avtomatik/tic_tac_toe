99e55c41500e2e74c2ea00034363bce8da32b8a4
Alexander <kkruscherr@gmail.com>
2024-02-18 19:36:21 +0300

    Initial Commit

---
+++ game.py
@@ -0,0 +1,68 @@
+from gameparts.exceptions import CellOccupiedError, FieldIndexError
+from gameparts.parts import Board
+
+
+def save_result(string):
+    with open('results.txt', 'a') as f:
+        f.write(f'{string}\n')
+
+
+def main():
+    game = Board()
+    current_player = 'X'
+    running = True
+    game.display()
+
+    while running:
+
+        print(f'Ход делают {current_player}')
+
+        while True:
+            try:
+                row = int(input('Введите номер строки: '))
+                if row < 0 or row >= game.field_size:
+                    raise FieldIndexError
+                column = int(input('Введите номер столбца: '))
+                if column < 0 or column >= game.field_size:
+                    raise FieldIndexError
+                if game.board[row][column] != ' ':
+                    raise CellOccupiedError
+            except FieldIndexError:
+                print(
+                    'Значение должно быть неотрицательным и меньше '
+                    f'{game.field_size}.'
+                )
+                print('Введите значения для строки и столбца заново.')
+                continue
+            except CellOccupiedError:
+                print('Ячейка занята.')
+                print('Пожалуйста, введите другие координаты.')
+                continue
+            except ValueError:
+                print('Буквы вводить нельзя. Только числа.')
+                print('Введите значения для строки и столбца заново.')
+                continue
+            except Exception as e:
+                print(f'Возникла ошибка: {e}')
+            else:
+                break
+
+        game.make_move(row, column, current_player)
+        game.display()
+        # После каждого хода надо делать проверку на победу и на ничью.
+        if game.check_win(current_player):
+            result = f'Победили {current_player}!'
+            running = False
+            print(result)
+            save_result(result)
+        elif game.is_board_full():
+            result = 'Ничья!'
+            running = False
+            print(result)
+            save_result(result)
+
+        current_player = 'O' if current_player == 'X' else 'X'
+
+
+if __name__ == '__main__':
+    main()
